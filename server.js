e8b6f4f21f5bb1a452e5a8bd64c6e9117be9874f
jack5972 <meetp6041@gmail.com>
2019-08-02 01:14:33 +0530

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const UserController = require('./controllers/user.controller')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const server;
+const http = require('http');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
@@ -15,9 +15,9 @@ app.use(cors())
 server = http.createServer(function(req,res){
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
-}
+})
 app.use('/',UserController)
 
 // app.use((req,res,next)=>{
