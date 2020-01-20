8e7164154ba752c36a768384dc253a609ca6013f
jack5972 <meetp6041@gmail.com>
2020-01-20 20:31:46 +0530

    'commit'

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -7,6 +7,8 @@ const path   = require('path');
 const mongoose = require('mongoose')
 const Image = mongoose.model('Image')
 
+router.use(express.static('public'));
+
 const storage = multer.diskStorage({
     destination: './public',
     filename: function(req, file, fn){
