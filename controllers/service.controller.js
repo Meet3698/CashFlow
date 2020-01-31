7f65de214903d0c69c0adc061c3fab880cfc543c
jack5972 <meetp6041@gmail.com>
2020-02-01 00:42:33 +0530

    'commit'

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -11,6 +11,7 @@ router.post('/add',(req,res)=>{
         console.log(item);
         const serv = new Service(item)
         serv.save((err)=>{
+            console.log(err);
             if(err)
             {
                 if(err.keyPattern.number == 1)
