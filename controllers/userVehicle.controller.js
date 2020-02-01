013458b2f45074addee546b9f404d7a9f2e1279c
jack5972 <meetp6041@gmail.com>
2020-02-01 16:28:40 +0530

    'commit'

--- controllers/userVehicle.controller.js
+++ controllers/userVehicle.controller.js
@@ -10,8 +10,8 @@ router.get('/',(req,res)=>{
 })
 
 router.post('/addvehicle',async (req,res)=>{
-    const uservehicle = new UserVehicle(req.body)
-    await uservehicle.save((err)=>{
+    const userVehicle = req.body
+    UserVehicle.collection.insertMany(userVehicle,(err,data)=>{
         if(err)
         {
             if(err.keyPattern.number == 1)
