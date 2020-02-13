6ab1081a46ad0c3febcea778a82034ae8bc2b3a7
jack5972 <meetp6041@gmail.com>
2020-02-13 16:31:44 +0530

    'commit'

--- controllers/package.controller.js
+++ controllers/package.controller.js
@@ -8,6 +8,24 @@ router.get('/',(req,res)=>{
     res.sendfile('./view/index.html')
 })
 
+
+router.get('/show',async(req,res)=>{
+    try {
+        Package.find({}).then((err,data)=>{
+            if(err){
+                res.json(err)
+           }
+            else{
+               res.json(data)
+                await  
+            }
+       })
+        } catch (e) {
+            const err = new Error(e)
+            log(err.message)
+    }
+})
+
 router.post('/addpack',async (req,res)=>{
     const package = new Package(req.body)
     await package.save()
