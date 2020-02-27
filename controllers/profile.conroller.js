30ce6f67a3751d2e575263b2fe46056c5bb96fa7
jack5972 <meetp6041@gmail.com>
2020-02-27 22:08:15 +0530

    'commit'

--- controllers/profile.conroller.js
+++ controllers/profile.conroller.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose')
 const Service = mongoose.model('Service')
 const User = mongoose.model('User')
 
-router.post('/userdetails',async(req,res)=>{
+router.post('/userdetails',(req,res)=>{
     console.log(req.body.email);
-    
     User.find({email:req.body.email}).then((data)=>{
+        console.log(data);
+        
         res.json({email : data.email,phone : data.phone})
     })
 })
