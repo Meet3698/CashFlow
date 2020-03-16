ba7d16f7f803450db90c70df1980447b7006b8d3
jack5972 <meetp6041@gmail.com>
2020-03-16 22:06:22 +0530

    'commit'

--- controllers/batch_making.controller.js
+++ controllers/batch_making.controller.js
@@ -7,46 +7,72 @@ const Cleaner = mongoose.model('Cleaner')
 const Package = mongoose.model('Package')
 
 router.post('/',async(req,res)=>{
-    
-    const arr = await Cleaner.collection.find({flag:0}).toArray()
-    const service = await Service.collection.find({flag : 0}).toArray()
-    
-    let len = arr.length-1
-    let len1 = service.length 
     let cust = []
-   
-    for (i=0;i<len1;i++)
-    {
-        if(len >= 0)
-        {   
-            // let offset =((new Date().getTimezoneOffset() / 60) * -1)
-            let time = new Date().getHours() + 6
-
-            console.log("Time : ",time)
-            
-            const arr1 = await UserVehicle.collection.find({number : service[i].number,prefferedTime:time}).toArray()
-            const pack = await Package.collection.find({packageId : service[i].id}).toArray()
-            console.log(arr1)
-            
-            if (arr1.length)
-            {
-                await Service.collection.updateOne(
-                    {number :  service[i].number},
-                    {$set : { flag : 1}}
-                )
-                
-                await Cleaner.collection.updateOne(
-                    {email:arr[len].email},
-                    {$set : { flag : 1}})
-
-                cust.push({cleaner : arr[len].name, package : pack, customer : arr1[0]})
-            }
-            len = len - 1
-        }
-    }
-    console.log(cust);
+    const email = req.body.email
+    const cleaner = await Cleaner.find({ $and : [{email : email},{flag : 0}]})
+    const service = await Service.find({flag:0})
+    const len = service.length -1
     
-    res.send(cust)
+    const package = await Package.find({packageId:service[len].id})
+    
+    if(len >0)
+    {
+        let time = new Date().getHours() 
+        console.log(time);
+        
+        const vehicle = await UserVehicle.find({number : service[len].number,prefferedTime:time})
+      
+        await Service.collection.updateOne(
+            {number :  service[len].number},
+            {$set : { flag : 1}}
+        )
+        
+        await Cleaner.collection.updateOne(
+            {email:email},
+            {$set : { flag : 1}})
+        
+        cust.push({cleaner : cleaner, vehicle : vehicle, package : package})
+    }
+    res.send({list : cust})
+    // const arr = await Cleaner.collection.find({flag:0}).toArray()
+    // const service = await Service.collection.find({flag : 0}).toArray()
+    
+    // let len = arr.length-1
+    // let len1 = service.length 
+    // let cust = []
+    
+    
+    // for (i=0;i<len1;i++)
+    // {
+    //     if(len >= 0)
+    //     {   
+    //         // let offset =((new Date().getTimezoneOffset() / 60) * -1)
+    //         let time = new Date().getHours() + 6
+
+    //         console.log("Time : ",time)
+            
+    //         const arr1 = await UserVehicle.collection.find({number : service[i].number,prefferedTime:time}).toArray()
+    //         const pack = await Package.collection.find({packageId : service[i].id}).toArray()
+    //         console.log(arr1)
+            
+    //         if (arr1.length)
+    //         {
+    //             await Service.collection.updateOne(
+    //                 {number :  service[i].number},
+    //                 {$set : { flag : 1}}
+    //             )
+                
+    //             await Cleaner.collection.updateOne(
+    //                 {email:arr[len].email},
+    //                 {$set : { flag : 1}})
+
+    //             cust.push({cleaner : arr[len].name, package : pack, customer : arr1[0]})
+    //         }
+    //         len = len - 1
+    //     }
+    // }
+    
+    // res.send(cust)
 })
 //---------------------------------------------------------------------
 router.post('/flag',async(req,res)=>{
