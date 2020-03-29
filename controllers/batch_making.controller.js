44b215f9c075032b5555a2b6a461fb8c205d888d
jack5972 <meetp6041@gmail.com>
2020-03-29 21:32:32 +0530

    'commit'

--- controllers/batch_making.controller.js
+++ controllers/batch_making.controller.js
@@ -17,7 +17,7 @@ router.post('/',async(req,res)=>{
     
     if(len >0 && len1>0)
     {
-        let time = new Date().getHours() + 6
+        let time = new Date().getHours() + 5.5
         console.log(time);
         
         for(i=0;i<len;i++)
