ea2461ffeb0ffcab576ec0a4823a5ce30e9c28bc
jack5972 <meetp6041@gmail.com>
2020-04-03 12:23:52 +0530

    'commit'

--- controllers/profile.conroller.js
+++ controllers/profile.conroller.js
@@ -6,6 +6,7 @@ const Service = mongoose.model('Service')
 const User = mongoose.model('User')
 const Package = mongoose.model('Package')
 const UserVehicle = mongoose.model('UserVehicle')
+const Code = mongoose.model('Code')
 
 router.post('/userdetails',(req,res)=>{
     User.find({email:req.body.email}).then((data)=>{
@@ -23,7 +24,8 @@ router.post("/history",async(req,res)=>{
     for (i = 0; i < len; i++) {
         const res = await Package.collection.find({packageId:id[i].id}).toArray()
         const cust = await UserVehicle.collection.find({number : id[i].number}).toArray()
-        pack.push({package : res, customer : cust})
+        const code = await Code.collection.find({id : id[i].id}).toArray
+        pack.push({package : res, customer : cust, code : code})
     }
     
     res.send(pack)
