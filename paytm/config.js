9b7113ca1a98cd916c1d56ef5b82b50ffabbed39
jack5972 <meetp6041@gmail.com>
2020-06-02 00:15:38 +0530

    'commit'

--- paytm/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MID: 'vcauMr24482835714726',
-  PAYTM_MERCHANT_KEY: 'NUv2T%zfPzhukKaY',
-  PAYTM_FINAL_URL: 'https://securegw-stage.paytm.in/order/process',
-  WEBSITE: 'WEBSTAGING',
-  CHANNEL_ID: 'WEB',
-  INDUSTRY_TYPE_ID: 'Retail',
-  CALLBACK_URL: 'http://localhost:3000/payment/response'
-};
